3b51ebe21fc736fefeb2b4b24d5db058e5d76b0d
David Spies <david.spies@leapyear.io>
2018-10-30 23:03:18 -0700

    Demote instancees

--- src/PersistWrap/Structure/Type.hs
+++ src/PersistWrap/Structure/Type.hs
@@ -2,6 +2,7 @@ module PersistWrap.Structure.Type where
 
 import Data.Promotion.Prelude (Symbol)
 import Data.Singletons
+import Data.Text (Text)
 
 import PersistWrap.Structure.Primitives
 
@@ -12,6 +13,13 @@ data Structure
   | ProductType [(Symbol, Structure)]
   | ListType Structure
   | MapType PrimName Structure
+data DStructure
+  = DPrimitive PrimName
+  | DUnitType
+  | DSumType (Text, DStructure) [(Text, DStructure)]
+  | DProductType [(Text, DStructure)]
+  | DListType DStructure
+  | DMapType PrimName DStructure
 
 data instance Sing (struct :: Structure) where
   SPrimitive :: SPrimName pn -> Sing ('Primitive pn)
@@ -33,5 +41,26 @@ instance SingI s => SingI ('ListType s) where
   sing = SListType sing
 instance (SingI k, SingI v) => SingI ('MapType k v) where
   sing = SMapType sing sing
+instance SingKind Structure where
+  type Demote Structure = DStructure
+  fromSing = \case
+    SPrimitive pn -> DPrimitive (fromSing pn)
+    SUnitType -> DUnitType
+    SSumType xHead xTail -> DSumType (fromSing xHead) (fromSing xTail)
+    SProductType xs -> DProductType (fromSing xs)
+    SListType x -> DListType (fromSing x)
+    SMapType k v -> DMapType (fromSing k) (fromSing v)
+  toSing = \case
+    DPrimitive n -> case toSing n of
+      SomeSing sn -> SomeSing $ SPrimitive sn
+    DUnitType -> SomeSing SUnitType
+    DSumType xHead xTail -> case (toSing xHead, toSing xTail) of
+      (SomeSing sxHead, SomeSing sxTail) -> SomeSing $ SSumType sxHead sxTail
+    DProductType xs -> case toSing xs of
+      SomeSing sxs -> SomeSing $ SProductType sxs
+    DListType x -> case toSing x of
+      SomeSing sx -> SomeSing $ SListType sx
+    DMapType k v -> case (toSing k, toSing v) of
+      (SomeSing sk, SomeSing sv) -> SomeSing $ SMapType sk sv
 
 type SStructure x = Sing (x :: Structure)
--- src/PersistWrap/Table/Column.hs
+++ src/PersistWrap/Table/Column.hs
@@ -7,12 +7,16 @@ import Data.Singletons (Sing, SingI(..))
 import Data.Singletons.Prelude
 import Data.Singletons.TH (singDecideInstances, singEqInstances, singOrdInstances, singletonsOnly)
 import Data.Singletons.TypeLits (SSymbol, Symbol)
+import Data.Text (Text)
 
 import PersistWrap.Structure (PrimName, SPrimName)
 
 data BaseColumn = Prim PrimName | Enum Symbol [Symbol] | ForeignKey Symbol | JSON
+data DBaseColumn = DPrim PrimName | DEnum Text [Text] | DForeignKey Text | DJSON
 data Column = Column Bool BaseColumn
+data DColumn = DColumn Bool DBaseColumn
 data Schema = Schema Symbol [(Symbol,Column)]
+data DSchema = DSchema Text [(Text,DColumn)]
 
 data instance Sing (bc :: BaseColumn) where
   SPrim :: SPrimName pn -> Sing ('Prim pn)
@@ -28,6 +32,21 @@ instance SingI n => SingI ('ForeignKey n) where
 instance SingI 'JSON where
   sing = SJSON
 type SBaseColumn x = Sing (x :: BaseColumn)
+instance SingKind BaseColumn where
+  type Demote BaseColumn = DBaseColumn
+  fromSing = \case
+    SPrim pn -> DPrim $ fromSing pn
+    SEnum x xs -> DEnum (fromSing x) (fromSing xs)
+    SForeignKey n -> DForeignKey (fromSing n)
+    SJSON -> DJSON
+  toSing = \case
+    DPrim pn -> case toSing pn of
+      SomeSing spn -> SomeSing $ SPrim spn
+    DEnum x xs -> case (toSing x, toSing xs) of
+      (SomeSing sx, SomeSing sxs) -> SomeSing $ SEnum sx sxs
+    DForeignKey n -> case toSing n of
+      SomeSing sn -> SomeSing $ SForeignKey sn
+    DJSON -> SomeSing SJSON
 
 data instance Sing (x :: Column) where
   SColumn
@@ -37,12 +56,22 @@ data instance Sing (x :: Column) where
 instance (SingI nullability, SingI ctype) => SingI ('Column nullability ctype) where
   sing = SColumn sing sing
 type SColumn x = Sing (x :: Column)
+instance SingKind Column where
+  type Demote Column = DColumn
+  fromSing (SColumn n bc) = DColumn (fromSing n) (fromSing bc)
+  toSing (DColumn n bc) = case (toSing n, toSing bc) of
+    (SomeSing sn, SomeSing sbc) -> SomeSing $ SColumn sn sbc
 
 data instance Sing (sch :: Schema) where
   SSchema :: SSymbol name -> SList cols -> Sing ('Schema name cols)
 instance (SingI name, SingI cols) => SingI ('Schema name cols) where
   sing = SSchema sing sing
 type SSchema x = Sing (x :: Schema)
+instance SingKind Schema where
+  type Demote Schema = DSchema
+  fromSing (SSchema n cols) = DSchema (fromSing n) (fromSing cols)
+  toSing (DSchema n cols) = case (toSing n, toSing cols) of
+    (SomeSing sn, SomeSing scols) -> SomeSing $ SSchema sn scols
 
 $(singDecideInstances [''BaseColumn, ''Column, ''Schema])
 $(singEqInstances [''BaseColumn, ''Column, ''Schema])
