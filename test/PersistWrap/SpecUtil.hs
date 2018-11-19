dd8482a871fd131619b6a65202d46140fb9a8a52
David Spies <david.spies@leapyear.io>
2018-11-19 15:29:44 -0800

    Move testing stuff around

---
+++ test/PersistWrap/SpecUtil.hs
@@ -0,0 +1,23 @@
+module PersistWrap.SpecUtil
+    ( shouldBeIgnoreOrder
+    ) where
+
+import Test.Hspec
+
+import Control.Monad (forM_)
+import Control.Monad.State (execStateT)
+import qualified Control.Monad.State as State
+import Data.List (find)
+import GHC.Stack (HasCallStack)
+
+removeInd :: Int -> [a] -> [a]
+removeInd i xs = let (xsbefore, xsafter) = splitAt i xs in xsbefore ++ tail xsafter
+
+sameElements :: forall a . Eq a => [a] -> [a] -> Bool
+sameElements xs ys = maybe False null $ (`execStateT` ys) $ forM_ xs $ \x -> do
+  ys'    <- State.get
+  (i, _) <- State.lift $ find ((== x) . snd) (zip [0 ..] ys')
+  State.put $ removeInd i ys'
+
+shouldBeIgnoreOrder :: (HasCallStack, Eq a, Show a) => [a] -> [a] -> Expectation
+shouldBeIgnoreOrder x y = (x, y) `shouldSatisfy` uncurry sameElements
