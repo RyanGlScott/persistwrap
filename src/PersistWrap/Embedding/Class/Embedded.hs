ca1ad5b402a15766d306e2f8ddacb1ab72f0bf68
David Spies <david.spies@leapyear.io>
2018-11-20 11:13:13 -0800

    Embedded monad

--- src/PersistWrap/Embedding/Class/Embedded.hs
+++ src/PersistWrap/Embedding/Class/Embedded.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module PersistWrap.Embedding.Class.Embedded where
 
+import Data.Function.Pointless ((.:))
+import Data.Promotion.Prelude (type (==))
 import Data.Singletons.TypeLits (Symbol)
 
 import PersistWrap.Embedding.Class.Embeddable (Embeddable)
@@ -21,5 +26,25 @@ stateX = E.stateX
 modifyX :: Embedded schemaName x m => ForeignKey m schemaName -> (x -> x) -> m Bool
 modifyX = E.modifyX
 
-newtype Itemized (items :: [(Symbol, *)]) m x = Itemized (m x)
+newtype Itemized (items :: [(Symbol, *)]) m x = Itemized {runItemized :: m x}
   deriving (Functor, Applicative, Monad, MonadTransactable)
+
+instance Embeddable schemaName x m => Embeddable schemaName x (Itemized items m) where
+  xSchemas = E.xSchemas @schemaName @x @m
+  getXs = Itemized E.getXs
+  getX = Itemized . E.getX
+  insertX = Itemized . E.insertX
+  deleteX = Itemized . E.deleteX @_ @x
+  stateX = Itemized .: E.stateX
+  modifyX = Itemized .: E.modifyX
+
+instance (Embeddable schemaName x m, MapsTo schemaName x items)
+  => Embedded schemaName x (Itemized items m)
+
+class MapsTo (schemaName :: Symbol) x (items :: [(Symbol, *)]) | schemaName items -> x
+instance MapsToH (schemaName == headName) schemaName x ('(headName, headX) ': rest)
+  => MapsTo schemaName x ('(headName, headX) ': rest)
+class MapsToH (current :: Bool) (schemaName :: Symbol) x (items :: [(Symbol, *)])
+  | schemaName items -> x
+instance MapsTo schemaName x rest => MapsToH 'False schemaName x (head ': rest)
+instance x ~ headX => MapsToH 'True schemaName x ('(headName, headX) ': rest)
--- test/PersistWrap/WidgetSpec.hs
+++ test/PersistWrap/WidgetSpec.hs
@@ -7,7 +7,8 @@ import qualified Data.ByteString.Char8 as BS
 import Data.Singletons
 import Test.Hspec
 
-import PersistWrap.Embedding.Class.Embeddable
+import PersistWrap.Embedding.Class.Embeddable (entitySchemas)
+import PersistWrap.Embedding.Class.Embedded
 import PersistWrap.Structure
 import PersistWrap.Table
 import qualified PersistWrap.Table.BackEnd.TVar as BackEnd
@@ -19,26 +20,26 @@ spec = describe "Widget" $ it "should get back what you put in" $ do
   let widgetSchemas = entitySchemas @BackEnd.FK @"widget" @(StructureOf (Widget BackEnd.FK))
       intSchemas    = entitySchemas @BackEnd.FK @"abc" @(StructureOf Int)
   case toSing (widgetSchemas ++ intSchemas) of
-    (SomeSing sSchemas) -> join $ BackEnd.withEmptyTables sSchemas $ \_ -> atomicTransaction $ do
-      fk3 <- insertX @"abc" (toEntity (3 :: Int))
-      let
-        w1, w2, w3 :: Widget BackEnd.FK
-        w1 = Blarg (False, True, BS.pack "hello world")
-        w2 = Glorp fk3
-        w3 =
-          Foo2
-            [ Foo { bar = 10, baz = A 3 4, qux = Just Green }
-            , Foo { bar = 11, baz = B, qux = Nothing }
-            ]
-      fkw1      <- insertX @"widget" w1
-      fkw2      <- insertX @"widget" w2
-      fkw3      <- insertX @"widget" w3
-      resultABC <- getX fk3
-      result1   <- getX fkw1
-      result2   <- getX fkw2
-      result3   <- getX fkw3
-      return $ do
-        resultABC `shouldBe` Just (3 :: Int)
-        result1 `shouldBe` Just w1
-        result2 `shouldBe` Just w2
-        result3 `shouldBe` Just w3
+    (SomeSing sSchemas) -> join $ BackEnd.withEmptyTables sSchemas $ \_ ->
+      atomicTransaction $ runItemized @'[ '("abc", Int), '("widget", Widget BackEnd.FK)] $ do
+        fk3 <- insertX @"abc" 3
+        let
+          w1 = Blarg (False, True, BS.pack "hello world")
+          w2 = Glorp fk3
+          w3 =
+            Foo2
+              [ Foo { bar = 10, baz = A 3 4, qux = Just Green }
+              , Foo { bar = 11, baz = B, qux = Nothing }
+              ]
+        fkw1      <- insertX @"widget" w1
+        fkw2      <- insertX @"widget" w2
+        fkw3      <- insertX @"widget" w3
+        resultABC <- getX fk3
+        result1   <- getX fkw1
+        result2   <- getX fkw2
+        result3   <- getX fkw3
+        return $ do
+          resultABC `shouldBe` Just 3
+          result1 `shouldBe` Just w1
+          result2 `shouldBe` Just w2
+          result3 `shouldBe` Just w3
