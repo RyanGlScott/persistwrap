403b3c52ec634d78c08afed0fae55997e2f6d660
David Spies <david.spies@leapyear.io>
2018-11-20 15:49:53 -0800

    Lift more things

--- src/PersistWrap/Embedding/Class/Embedded.hs
+++ src/PersistWrap/Embedding/Class/Embedded.hs
@@ -4,7 +4,11 @@
 module PersistWrap.Embedding.Class.Embedded where
 
 import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Except (MonadError)
+import Control.Monad.Reader (MonadReader)
+import Control.Monad.State (MonadState)
 import Control.Monad.Trans (MonadTrans(..))
+import Control.Monad.Writer (MonadWriter)
 import Data.Function.Pointless ((.:))
 import Data.Promotion.Prelude (type (==))
 import Data.Singletons.TypeLits (Symbol)
@@ -29,7 +33,9 @@ modifyX :: Embedded schemaName x m => ForeignKey m schemaName -> (x -> x) -> m B
 modifyX = E.modifyX
 
 newtype Itemized (items :: [(Symbol, *)]) m x = Itemized {runItemized :: m x}
-  deriving (Functor, Applicative, Monad, MonadTransactable, MonadIO)
+  deriving ( Functor, Applicative, Monad, MonadTransactable, MonadIO
+           , MonadReader r, MonadWriter w, MonadState s, MonadError e
+           )
 instance MonadTrans (Itemized items) where
   lift = Itemized
 instance MonadDML m => MonadDML (Itemized items m) where
