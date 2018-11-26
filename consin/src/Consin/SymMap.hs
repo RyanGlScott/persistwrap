1c08fd547f7bd51a68314f4743a4412c25bc10b2
David Spies <dnspies@gmail.com>
2018-11-26 07:57:48 -0800

    Generalize SymMap key

--- consin/src/Consin/SymMap.hs
+++ consin/src/Consin/SymMap.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeInType #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module Consin.SymMap
@@ -17,17 +20,19 @@ module Consin.SymMap
 
 import Prelude hiding (lookup)
 
+import Data.Kind (type (*))
 import qualified Data.Map as Map
 import Data.Singletons (Sing, SomeSing(SomeSing), sing)
-import Data.Singletons.Decide ((:~:)(Refl), Decision(..), (%~))
-import Data.Singletons.TypeLits (SSymbol, Symbol)
+import Data.Singletons.Decide ((:~:)(Refl), Decision(..), SDecide, (%~))
+import Data.Singletons.ShowSing (ShowSing)
+import Data.Singletons.Prelude (SOrd)
 
 import Consin.Class (AlwaysS)
 import Consin.Some
 
-newtype SymMap (v :: Symbol -> *) = SymMap (Map.Map (SomeSing Symbol) (Some v))
+newtype SymMap (v :: k -> *) = SymMap (Map.Map (SomeSing k) (Some v))
 
-deriving instance AlwaysS Show v => Show (SymMap v)
+deriving instance (ShowSing k, AlwaysS Show v) => Show (SymMap (v :: k -> *))
 
 empty :: SymMap v
 empty = SymMap Map.empty
@@ -35,20 +40,20 @@ empty = SymMap Map.empty
 singleton :: forall x v . Sing x -> v x -> SymMap v
 singleton k = SymMap . Map.singleton (SomeSing k) . some k
 
-union :: SymMap v -> SymMap v -> SymMap v
+union :: SOrd k => SymMap (v :: k -> *) -> SymMap v -> SymMap v
 union (SymMap x) (SymMap y) = SymMap $ Map.union x y
 
-difference, (\\) :: SymMap v -> SymMap v -> SymMap v
+difference, (\\) :: SOrd k => SymMap (v :: k -> *) -> SymMap v -> SymMap v
 difference (SymMap x) (SymMap y) = SymMap $ Map.difference x y
 (\\) = difference
 
-insert :: forall x v . Sing x -> v x -> SymMap v -> SymMap v
+insert :: forall k (x :: k) v . SOrd k => Sing x -> v x -> SymMap v -> SymMap v
 insert k v (SymMap m) = SymMap $ Map.insert (SomeSing k) (some k v) m
 
-delete :: SSymbol x -> SymMap v -> SymMap v
+delete :: SOrd k => Sing (x :: k) -> SymMap (v :: k -> *) -> SymMap v
 delete sx (SymMap m) = SymMap $ Map.delete (SomeSing sx) m
 
-lookup :: forall x v . SSymbol x -> SymMap v -> Maybe (v x)
+lookup :: forall k (x :: k) v . (SDecide k, SOrd k) => Sing x -> SymMap v -> Maybe (v x)
 lookup k (SymMap m) =
   (\(Some (v :: v x')) -> case sing @_ @x' %~ k of
       Disproved{} -> error "Bad value in Map"
@@ -56,10 +61,10 @@ lookup k (SymMap m) =
     )
     <$> Map.lookup (SomeSing k) m
 
-(!?) :: forall x v . SymMap v -> SSymbol x -> Maybe (v x)
+(!?) :: forall k (x :: k) v . (SDecide k, SOrd k) => SymMap v -> Sing x -> Maybe (v x)
 (!?) = flip lookup
 
-fromList :: forall v . [Some v] -> SymMap v
+fromList :: forall k (v :: k -> *) . SOrd k => [Some v] -> SymMap v
 fromList = SymMap . Map.fromList . map (\(Some (v :: v x)) -> (SomeSing $ sing @_ @x, Some v))
 
 toList :: forall v . SymMap v -> [Some v]
