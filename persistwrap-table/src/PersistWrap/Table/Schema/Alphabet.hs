22b3ba5c9b6e008831672156ee4688bd543bab24
David Spies <david.spies@leapyear.io>
2019-01-21 06:12:39 -0800

    Add Arbitrary instances

---
+++ persistwrap-table/src/PersistWrap/Table/Schema/Alphabet.hs
@@ -0,0 +1,35 @@
+module PersistWrap.Table.Schema.Alphabet
+    ( natoAlphabet
+    ) where
+
+import Data.Text (Text)
+
+natoAlphabet :: [Text]
+natoAlphabet =
+  [ "Alfa"
+  , "Bravo"
+  , "Charlie"
+  , "Delta"
+  , "Echo"
+  , "Foxtrot"
+  , "Golf"
+  , "Hotel"
+  , "India"
+  , "Juliett"
+  , "Kilo"
+  , "Lima"
+  , "Mike"
+  , "November"
+  , "Oscar"
+  , "Papa"
+  , "Quebec"
+  , "Romeo"
+  , "Sierra"
+  , "Tango"
+  , "Uniform"
+  , "Victor"
+  , "Whiskey"
+  , "X-ray"
+  , "Yankee"
+  , "Zulu"
+  ]
--- persistwrap-table/src/PersistWrap/Table/Schema/Internal.hs
+++ persistwrap-table/src/PersistWrap/Table/Schema/Internal.hs
@@ -7,14 +7,20 @@
 
 module PersistWrap.Table.Schema.Internal where
 
+import Control.Applicative (empty)
+import Data.List (group, sort)
+import qualified Data.List.NonEmpty as NonEmpty
 import Data.Text (Text)
+import qualified Data.Text as Text
 import qualified Language.Haskell.TH.Lift as TH
 import Data.List.NonEmpty (NonEmpty)
 import Data.Singletons.Prelude
 import Data.Singletons.TH
+import Test.QuickCheck (Arbitrary(..), Gen, elements, listOf1, oneof, shrinkList)
 
 import Language.Haskell.TH.PromotedLift (PromotedLift(..))
 import PersistWrap.Primitives (PrimName)
+import PersistWrap.Table.Schema.Alphabet
 import PersistWrap.Table.TH.Lift.Orphans ()
 
 $(singletons [d|
@@ -52,3 +58,28 @@ instance PromotedLift (Schema Symbol) where
 type TabSchema (tab :: (*, Schema Symbol)) = Snd tab
 type TabName tab = SchemaName (TabSchema tab)
 type TabCols tab = SchemaCols (TabSchema tab)
+
+dedupSort :: Ord a => [a] -> [a]
+dedupSort = map head . group . sort
+
+arbitraryEnumOpts :: Gen (NonEmpty Text)
+arbitraryEnumOpts = NonEmpty.fromList . dedupSort <$> listOf1 (elements natoAlphabet)
+
+instance Arbitrary (BaseColumn Text) where
+  arbitrary = oneof [Prim <$> arbitrary, Enum <$> arbitraryEnumOpts, pure JSON] -- TODO Foreign keys
+  shrink    = \case
+    Prim       p -> Prim <$> shrink p
+    Enum       _ -> empty
+    ForeignKey _ -> empty
+    JSON         -> empty
+instance Arbitrary (Column Text) where
+  arbitrary = Column <$> arbitrary <*> arbitrary
+  shrink (Column n bc) = Column n <$> shrink bc
+
+genSchema :: Gen (Text -> Schema Text)
+genSchema =
+  (\cols name -> Schema name $ zip (map (Text.pack . ("col" ++) . show) [(0 :: Int) ..]) cols)
+    <$> arbitrary
+
+shrinkSchema :: Schema Text -> [Schema Text]
+shrinkSchema (Schema name cols) = Schema name <$> shrinkList (const []) cols
